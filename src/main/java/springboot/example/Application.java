76a39c940f69983249a4dd4f6fbc61039ace5b0f
kisshello <1109279069@qq.com>
2018-08-13 18:08:08 +0800

    first commit

---
+++ src/main/java/springboot/example/Application.java
@@ -0,0 +1,15 @@
+package springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    @RequestMapping("/")
+    String home(){
+        return "hello";
+    }
+}
